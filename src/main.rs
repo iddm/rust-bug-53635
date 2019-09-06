95fff931aa83cfe3a480791d1c76d5d6bb39b6e8
Victor Polevoy <fx@thefx.co>
2019-09-06 18:43:00 +0200

    Simplify the code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-type Result<T> = std::result::Result<T, ()>;
-
-pub fn f<T>(f: &dyn Fn() -> Result<T>) -> Result<T> {
-    f()
-}
-
 // Either comment this line below with `rocket::post`...
 #[rocket::post("/")]
-fn open_session() -> Result<String> {
-    // ...or comment this if, or don't call "f" function
-    if let Ok(_) = f(&|| Ok(())) {
-    // This if also reproduces.
-    // if let Ok(_) = &|| Ok(()) {
-	return Ok("2".to_string())
+fn problem() -> String {
+    // If I surround the lambda with parens as below, everything is okay.
+    //
+    // if let Some(_) = (|| Some(())) {
+    //
+    // But if not, the code causing problems is not shown. Also, 
+    // instead of a lambda there can be a function call.
+    if let Some(_) = || Some(()) {
+	return "2".to_string()
     }
 
-    Ok("1")
+    // This should be the line causing the error.
+    1
 }
 
 fn main() {
